0c6cc832321d2910b99f2e9de5cd70f665c63d94
WuChenDi <wuchendi96@gmail.com>
2023-02-19 15:19:15 +0800

    feat: add wallet page

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  interface Window {
+    // ethereum: import('ethers').providers.ExternalProvider;
+    ethereum: any;
+  }
+}
+
+// import { MetaMaskInpageProvider } from "@metamask/providers";
+
+// declare global {
+//   interface Window {
+//     ethereum: MetaMaskInpageProvider;
+//   }
+// }
